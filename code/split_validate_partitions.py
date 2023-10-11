6336c041f2c87dbc44e15a7591c4c70d00a185e2
HuaiyanRen <928003585@qq.com>
2023-10-11 14:26:51 +1100

    Update split_validate_partitions.py
    Filter empty taxa

--- code/split_validate_partitions.py
+++ code/split_validate_partitions.py
@@ -1,6 +1,7 @@
 import random
 import csv
 import os
+import re
 
 random.seed(2023)
 
@@ -26,7 +27,34 @@ with open('validate.txt', 'w') as loci:
         
 os.system('python3 AMAS.py split -f nexus -d dna -i alignment.nex -l validate.txt -u nexus')
 
+def contains_required_letters(line):
+    for letter in ['A', 'C', 'G', 'T']:
+        if letter in line:
+            return True
+    return False
+
+order = 1
 for i in validate_list:
     old_name = 'alignment_' + names[i] + '-out.nex'
-    new_name =  names[i] + '.nex'
+    new_name = str(order) + '_' + names[i] + '.nex'
+    
+    with open(old_name, 'r') as file:
+        lines = file.readlines()
+    
+    filtered_lines = []
+    remove = 0
+    for idx, line in enumerate(lines, start=1):  
+        if 7 <= idx < 97 and not contains_required_letters(line.split()[1]):
+            remove += 1
+            continue
+        filtered_lines.append(line)
+    
+    filtered_lines[3] = re.sub(r'(NTAX=)\d+', r'NTAX=' + str(90 - remove), lines[3])
+    
+    with open(old_name, 'w') as file:
+        file.writelines(filtered_lines)
+    
     os.rename(old_name, new_name)
+    order += 1
+    
+    
