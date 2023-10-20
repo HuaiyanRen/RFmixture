bcbfbff5abb842ece3aa8d7c749dea17b2dbd310
HuaiyanRen <928003585@qq.com>
2023-10-20 13:46:04 +1100

    Update write_modelfile.py
    Add 1-class model and freerate models

--- code/write_modelfile.py
+++ code/write_modelfile.py
@@ -12,9 +12,48 @@ with open(modelfile,'w+') as f:
 
 for data_name in data_list:
     file_name = data_name + '_q' + str(classes) 
-    full_file_name = os.path.join(path, file_name + '.xlsx')
     
-    model_df = pd.read_excel(full_file_name)
+    one_file_name = os.path.join(path, data_name + '_mf.iqtree')
+    
+    if_invar = False
+    with open(one_file_name) as b:
+        for line in b.readlines():
+            if 'A-C:' in line:
+                AC1 = float(line.split()[-1])
+            if 'A-G:' in line:
+                AG1 = float(line.split()[-1])
+            if 'A-T:' in line:
+                AT1 = float(line.split()[-1])
+            if 'C-G:' in line:
+                CG1 = float(line.split()[-1])
+            if 'C-T:' in line:
+                CT1 = float(line.split()[-1])
+            if 'pi(A)' in line:
+                FA1 = float(line.split()[-1])
+            if 'pi(C)' in line:
+                FC1 = float(line.split()[-1])
+            if 'pi(G)' in line:
+                FG1 = float(line.split()[-1])
+            if 'pi(T)' in line:
+                FT1 = float(line.split()[-1])
+            if 'Proportion of invariable sites:' in line:
+                if_invar = True
+                invar = float(line.split()[-1])
+            if 'Site proportion and rates:' in line:
+                n_rate = line.count('(')
+                rate_str = line.split('(')[1].split(')')[0]
+                for i in range(2, n_rate+1):
+                    rate_str = rate_str + ',' + line.split('(')[i].split(')')[0]
+    
+    model_str_1 = 'GTR{'+str(AC1)+'/'+str(AG1) +'/'+str(AT1)+'/'+str(CG1)+'/'+str(CT1)+'}+F{'+str(FA1)+'/'+str(FC1)+'/'+str(FG1)+'/'+str(FT1)+'}'
+    if if_invar:
+        model_str_1 = model_str_1 + '+I{' + str(invar) + '}+R' + str(n_rate) + '{' + rate_str + '};'
+    else:
+        model_str_1 = model_str_1 + '+R' + str(n_rate) + '{' + rate_str + '};'
+    
+    mix_file_name = os.path.join(path, file_name + '.xlsx')
+    
+    model_df = pd.read_excel(mix_file_name)
     AC = model_df['AC'].tolist()
     AG = model_df['AG'].tolist()
     AT = model_df['AT'].tolist()
@@ -26,16 +65,20 @@ for data_name in data_list:
     FT = model_df['FT'].tolist()
     assert len(AC) == int(classes)
     
-    model_str ='MIX{'
+    model_str_mix ='MIX{'
     for i in range(int(classes)):
         if i == 0:
-            model_str = model_str + 'GTR{'+str(AC[i])+'/'+str(AG[i]) +'/'+str(AT[i])+'/'+str(CG[i])+'/'+str(CT[i])+'}+F{'+str(FA[i])+'/'+str(FC[i])+'/'+str(FG[i])+'/'+str(FT[i])+'}'
+            model_str_mix = model_str_mix + 'GTR{'+str(AC[i])+'/'+str(AG[i]) +'/'+str(AT[i])+'/'+str(CG[i])+'/'+str(CT[i])+'}+F{'+str(FA[i])+'/'+str(FC[i])+'/'+str(FG[i])+'/'+str(FT[i])+'}'
         else:
-            model_str = model_str + ',GTR{'+str(AC[i])+'/'+str(AG[i]) +'/'+str(AT[i])+'/'+str(CG[i])+'/'+str(CT[i])+'}+F{'+str(FA[i])+'/'+str(FC[i])+'/'+str(FG[i])+'/'+str(FT[i])+'}'
-    model_str = model_str + '}+'
+            model_str_mix = model_str_mix + ',GTR{'+str(AC[i])+'/'+str(AG[i]) +'/'+str(AT[i])+'/'+str(CG[i])+'/'+str(CT[i])+'}+F{'+str(FA[i])+'/'+str(FC[i])+'/'+str(FG[i])+'/'+str(FT[i])+'}'
+    if if_invar:
+        model_str_mix = model_str_mix + '}+I{' + str(invar) + '}+R' + str(n_rate) + '{' + rate_str + '};'
+    else:
+        model_str_mix = model_str_mix + '}+R' + str(n_rate) + '{' + rate_str + '};'
     
     with open(modelfile,'a+') as f:
-        f.write('\tmodel ' + file_name + ' = ' + model_str + ';\n') 
+        f.write('\tmodel ' + data_name + '_mf = ' + model_str_1 + '\n') 
+        f.write('\tmodel ' + file_name + ' = ' + model_str_mix + '\n') 
         
 with open(modelfile,'a+') as f:
     f.write('end;\n')
