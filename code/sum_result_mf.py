0747862ccfa79d21a6609b27be2ec3bb7cff531e
HuaiyanRen <928003585@qq.com>
2023-10-24 09:48:41 +1100

    Summarize Q10 validation results
    Bug fixed

--- code/sum_result_mf.py
+++ code/sum_result_mf.py
@@ -8,15 +8,15 @@ part_list = []
 for i in range(len(lines)):
     part_list.append(str(i+1) + '_' + lines[i].split()[0] + '.nex')   
     
-model_list = ['GTR+FU+R8', 'l1t90_1','l1t90_2','l2t90_1','l2t90_2','l5t90_1','l10t90_1','l10t90_2']
+model_list = ['GTR+FU+R8', 'l1t90_1','l1t90_2','l2t90_1','l2t90_2','l5t90_1','l5t90_2','l10t90_1','l10t90_2']
     
 with open('mf_results.csv','w+',newline='') as csvf:
     csv_write = csv.writer(csvf)
-    csv_write.writerow(['partition', 'q1', 'l1t90_1','l1t90_2','l2t90_1','l2t90_2','l5t90_1','l10t90_1','l10t90_2'])   
+    csv_write.writerow(['partition', 'q1', 'l1t90_1','l1t90_2','l2t90_1','l2t90_2','l5t90_1','l5t90_2','l10t90_1','l10t90_2'])   
     
     
 for part_name in part_list:
-    file_name = 'mf_dec/' + part_name + '.iqtree'
+    file_name = 'mf_dec2/' + part_name + '.iqtree'
     order_dict = {}
     current_order = 1
     with open(file_name, 'r') as file:
