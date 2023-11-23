e404f04ed24788b600f03f995ec4847b864d299b
HuaiyanRen <928003585@qq.com>
2023-11-23 11:20:40 +1100

    Update unroot_tree.py

--- code/unroot_tree.py
+++ code/unroot_tree.py
@@ -6,4 +6,4 @@ true_treestr = open(true_treefile,'r').read()
 true_tree = Tree(true_treestr,format = 1)
 true_tree.unroot()
 with open(r'C:\Users\u7151703\Desktop\research\RFmixture\data\mammal\trees\aa_t90_unrooted.treefile', 'w+') as result:
-    result.write(true_tree.write(format = 9) + '\n')    
+    result.write(true_tree.write(format = 5) + '\n')    
